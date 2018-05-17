01b3732e5c96b0b8d744224aed4493c0a3081238
Veselin Dimitrov <vidimitrov94@gmail.com>
2018-05-17 23:39:30 +0300

    Wired up all the redux setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
+/* eslint no-undef: 0 */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/app';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import {createLogger} from 'redux-logger';
+import {routerReducer} from 'react-router-redux';
+import * as reducers from './reducers';
+import Root from './containers/Root';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const middlewares = [thunk];
+if (process.env.NODE_ENV !== 'production') {
+  middlewares.push(createLogger());
+}
+
+export const store = createStore(
+  combineReducers({
+    ...reducers,
+    routing: routerReducer,
+  }),
+  compose(
+    applyMiddleware(...middlewares),
+    window.devToolsExtension &&
+      process.env.NODE_ENV === 'development' ?
+      window.devToolsExtension() : (f) => f,
+  ),
+);
+
+ReactDOM.render(
+  <Root store={store} />,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
