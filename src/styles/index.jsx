4d1192e49b58b2d79faa1d69fe8cec8350e4ee44
Veselin Dimitrov <vidimitrov94@gmail.com>
2018-05-17 23:38:26 +0300

    Add root level (theme) styles

---
+++ src/styles/index.jsx
@@ -0,0 +1,23 @@
+const colors = {
+  white: '#fff',
+  black: '#000',
+  gray: 'gray',
+};
+const wrapper = {
+  wrapper: {
+    height: '100%',
+  },
+};
+
+const shadow = {
+  boxShadow: 'inset 0px 200px 200px rgba(0, 0, 0, 0.9)',
+  width: '100%',
+  height: '100%',
+  position: 'absolute',
+};
+
+export {
+  colors,
+  wrapper,
+  shadow,
+};
