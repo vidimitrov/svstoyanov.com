2f2f98bae0c4149ccc44764c0467f3e5b6c6cd5d
Veselin Dimitrov <vidimitrov94@gmail.com>
2018-05-22 22:59:33 +0300

    Add global colors and typography styles

--- src/styles/index.jsx
+++ src/styles/index.jsx
@@ -1,17 +1,47 @@
 const colors = {
-  white: '#fff',
-  black: '#000',
-  gray: 'gray',
+  white: '#FFFFFF',
+  black: '#171717',
+  primary: '#49DDBA',
+  cian: '#02A0A7',
+  darkCian: 'rgba(73, 221, 186, 0.12)',
+  gray: 'rgba(255, 255, 255, 0.12)',
+  lighterGray: 'rgba(255, 255, 255, 0.48)',
+  darkerGray: 'rgba(255, 255, 255, 0.72)',
 };
 
-const shadow = {
-  boxShadow: 'inset 0px 200px 200px rgba(0, 0, 0, 0.9)',
-  width: '100%',
-  height: '100%',
-  position: 'absolute',
+const typography = {
+  h1: {
+    fontSize: '72px',
+    lineHeight: '108px',
+    fontWeight: 'bold',
+  },
+  h2: {
+    fontSize: '48px',
+    lineHeight: '72px',
+  },
+  h3: {
+    fontSize: '24px',
+    lineHeight: '36px',
+  },
+  h4: {
+    fontSize: '16px',
+    lineHeight: '24px',
+  },
+  h5: {
+    fontSize: '12px',
+    lineHeight: '18px',
+  },
+  buttons: {
+    fontSize: '16px',
+    lineHeight: '21px',
+  },
+  text: {
+    fontSize: '24px',
+    lineHeight: '36px',
+  },
 };
 
 export {
   colors,
-  shadow,
+  typography,
 };
