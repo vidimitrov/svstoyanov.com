93052c6dcb357c398d5de0936f2a80d3e9cef2f3
Veselin Dimitrov <vidimitrov94@gmail.com>
2018-05-26 10:48:58 +0300

    Add projects reducer

---
+++ src/reducers/projects.js
@@ -0,0 +1,12 @@
+import {
+
+} from '../constants/actionTypes';
+
+import {projectsInitialState} from './initialState';
+
+export default function projects(state = projectsInitialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
