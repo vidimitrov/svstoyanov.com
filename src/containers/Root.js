a34630897ae500335c4ee09ec2d403ffd6c07d73
Veselin Dimitrov <vidimitrov94@gmail.com>
2018-05-26 10:52:53 +0300

    Extract wrapper component from the routes

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Provider} from 'react-redux';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './Home/Home';
 import Project from '../components/Project/Project';
-import GridContainer from '../components/Grid/GridContainer';
 
 const Root = ({store}) => (
   <Provider store={store}>
     <Router>
-      <GridContainer className="wrapper" justify="space-between">
+      <Switch>
         <Route exact={true} path="/" component={Home} />
-        <Route path="/projects/:projectId" component={Project} />
-      </GridContainer>
+        <Route exact={true} path="/projects/:projectId" component={Project} />
+      </Switch>
     </Router>
   </Provider>
 );
