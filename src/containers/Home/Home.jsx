54d58bcceaf9bedc71b8bf93a0b2cabace8f60f7
Veselin Dimitrov <vidimitrov94@gmail.com>
2018-05-26 14:22:24 +0300

    Update Home container layout and styles

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {compose} from 'recompose';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router';
 import withStyles from 'material-ui/styles/withStyles';
 
-import GridContainer from '../../components/Grid/GridContainer';
-import ItemGrid from '../../components/Grid/ItemGrid';
+import Grid from 'material-ui/Grid';
 // import Preloader from '../../components/Preloader/Preloader';
 import Footer from '../../components/Footer/Footer';
 import Chat from '../Chat/Chat';
+import NavigationButton from '../../components/Buttons/NavigationButton';
 
 import style from './styles.jsx';
 import bgImage from '../../assets/img/bg.png';
@@ -14,27 +17,62 @@ import bgImage from '../../assets/img/bg.png';
 class Home extends React.Component {
   render() {
     const {classes} = this.props;
+    const {steps} = this.props;
 
     return (
-      <div className={classes.app}
+      <Grid container justify='flex-end'
+        className={classes.container}
         style={{backgroundImage: 'url(' + bgImage + ')'}}>
-        <GridContainer justify="space-between">
-          {/* <Preloader /> */}
-          <ItemGrid xs={12} sm={12} md={8} lg={8}>
-            <Chat />
-          </ItemGrid>
-          <ItemGrid xs={12} sm={12} md={12} lg={12}>
-            <Footer />
-          </ItemGrid>
-        </GridContainer>
-      </div>
+        {/* <Preloader /> */}
+        <Grid item xs={12}>
+          <Chat steps={steps} />
+        </Grid>
+        <Footer
+          content={
+            <Grid container justify='space-between'
+              className={classes.navigation} >
+              <Grid item xs={3} sm={3} md={3}>
+                <NavigationButton>
+                  ABOUT_ME
+                </NavigationButton>
+              </Grid>
+              <Grid item xs={3} sm={3} md={3}>
+                <NavigationButton>
+                  MY_WORK
+                </NavigationButton>
+              </Grid>
+              <Grid item xs={3} sm={3} md={3}>
+                <NavigationButton>
+                  CONTACT_ME
+                </NavigationButton>
+              </Grid>
+            </Grid>
+          } />
+      </Grid>
     );
   }
 }
 
 Home.propTypes = {
   classes: PropTypes.object.isRequired,
+  steps: PropTypes.array,
+  projects: PropTypes.array,
 };
 
+const mapStateToProps = (state, ownProps) => {
+  return {
+    steps: state.conversation.steps,
+    projects: state.projects,
+    routing: state.routing,
+  };
+};
 
-export default withStyles(style)(Home);
+const mapDispatchToProps = () => {
+  return {};
+};
+
+export default compose(
+  withRouter,
+  withStyles(style),
+  connect(mapStateToProps, mapDispatchToProps)
+)(Home);
--- src/containers/Home/styles.jsx
+++ src/containers/Home/styles.jsx
@@ -2,12 +2,19 @@
 import {colors} from '../../styles';
 
 export default {
-  app: {
-    boxSizing: 'border-box',
+  container: {
     fontFamily: 'Space Mono',
     background: 'rgba(0,0,0,0.1) url("../../../assets/bg.png") no-repeat center',
     backgroundBlendMode: 'multiply',
     color: colors.white,
+    flexGrow: 1,
     height: '100%',
+    paddingBottom: '15% !important',
+  },
+  navigation: {
+    textAlign: 'center',
+    maxWidth: '450px',
+    margin: '0 auto',
+    zIndex: 999,
   },
 };
