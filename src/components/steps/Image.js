334e93511870231d0944aabad0ada63c3a6f4b87
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-12-10 22:49:34 +0200

    Fix ImageStep styles

--- src/components/steps/Image.js
+++ src/components/steps/Image.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 
 const ImageStep = styled.img`
-  width: 400px;
+  width: 100%;
+  height: 100%;
+  border: 3px solid #02a0a7;
+  border-radius: 3px;
 
   @media (max-width: 420px) {
     width: 150px;
