b832ccdb2e0837a4a4f60a334b9ee9a70f8dd186
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-12-11 01:49:19 +0200

    Render dynamic custom options

--- src/components/CustomOptions/CustomOptions.jsx
+++ src/components/CustomOptions/CustomOptions.jsx
@@ -1,14 +1,20 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '../Buttons/Button';
 import styles from './styles';
 
-const CustomOptions = ({ classes, options, triggerNextStep }) => (
-  <Grid container className={classes.customOptionsWrapper} justify="flex-start">
-    {options.map((option, index) => (
+class CustomOptions extends React.Component {
+
+  render() {
+    const { classes, options, dynamicOptions, triggerNextStep } = this.props;
+    const filterOnlyVisible = (dynamicOption) => {
+      return dynamicOption.isVisible();
+    }
+    const renderOption = (option, index) => (
       <Button
         key={index}
         className={classes.optionsButton}
@@ -32,13 +38,26 @@ const CustomOptions = ({ classes, options, triggerNextStep }) => (
       >
         {option.label.split(' ').join('_').toUpperCase()}
       </Button>
-    ))}
-  </Grid>
-);
+    );
+    let dynamicOption;
+
+    if (dynamicOptions) {
+      dynamicOption = _.head(dynamicOptions.filter(filterOnlyVisible));
+    }
+
+    return (
+      <Grid container className={classes.customOptionsWrapper} justify="flex-start">
+        {options.map(renderOption)}
+        {dynamicOption && renderOption(dynamicOption, 10)}
+      </Grid>
+    )
+  };
+}
 
 CustomOptions.propTypes = {
   classes: PropTypes.object.isRequired,
   options: PropTypes.array.isRequired,
+  dynamicOptions: PropTypes.array,
   triggerNextStep: PropTypes.func.isRequired,
 };
 
