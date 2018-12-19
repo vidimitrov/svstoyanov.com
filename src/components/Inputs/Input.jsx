e2374b6873811eb9a4a799cc1339e042d650fc86
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-12-19 12:38:51 +0200

    Add arrow infront of Input component and add a submit button

--- src/components/Inputs/Input.jsx
+++ src/components/Inputs/Input.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import styles from './styles';
+import Button from '../Buttons/NavigationButton';
+import ArrowAvatar from '../Avatars/ArrowAvatar';
+import arrowAvatar from '../../assets/img/bot-arrow.svg';
 
 class Input extends React.Component {
   constructor(props) {
@@ -9,50 +13,82 @@ class Input extends React.Component {
 
     this.state = {
       value: '',
+      isButtonHidden: false,
     };
 
     this.onValueChange = this.onValueChange.bind(this);
+    this.submit = this.submit.bind(this);
   }
 
   onValueChange(e) {
     this.setState({ value: e.target.value });
   }
 
-  render() {
-    const { value } = this.state;
+  submit() {
     const {
-      classes,
       triggerNextStep,
       trigger,
-      placeholder,
       callback,
+    } = this.props;
+    const { value } = this.state;
+
+    triggerNextStep({
+      stepId: trigger,
+      externalTrigger: true,
+      value,
+    });
+
+    if (this.input) {
+      this.input.blur();
+      this.input.disabled = true;
+    }
+
+    if (callback) {
+      callback(value);
+    }
+
+    this.setState({
+      isButtonHidden: true,
+    });
+  }
+
+  render() {
+    const { value, isButtonHidden } = this.state;
+    const {
+      classes,
+      placeholder,
     } = this.props;
 
     return (
-      <input
-        type="text"
-        className={classes.input}
-        // eslint-disable-next-line no-return-assign
-        ref={inputRef => (this.input = inputRef)}
-        placeholder={placeholder}
-        value={value}
-        onChange={this.onValueChange}
-        onKeyPress={(e) => {
-          if (e.key === 'Enter') {
-            triggerNextStep({
-              stepId: trigger,
-              externalTrigger: true,
-              value,
-            });
-            this.input.blur();
-            this.input.disabled = true;
-
-            if (callback) {
-              callback(value);
-            }
-          }
-        }}
-      />
+      <Grid container>
+        <Grid item className={classes.avatarWrapper}>
+          <ArrowAvatar src={arrowAvatar} />
+        </Grid>
+        <Grid item className={classes.wrapper}>
+          <input
+            type="text"
+            className={classes.input}
+            // eslint-disable-next-line no-return-assign
+            ref={inputRef => (this.input = inputRef)}
+            placeholder={placeholder}
+            value={value}
+            onChange={this.onValueChange}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                this.submit();
+              }
+            }}
+          />
+        </Grid>
+        <Grid item>
+          <Button
+            onClick={this.submit}
+            disabled={!value}
+            hidden={isButtonHidden}>
+            SENT
+          </Button>
+        </Grid>
+      </Grid>
     );
   }
 }
