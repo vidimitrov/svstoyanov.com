5bbfe392c3a549d753989b2354afeca09d0148fe
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-12-19 12:38:00 +0200

    Refactor the NavigationButton component and add new functionality

--- src/components/Buttons/NavigationButton.jsx
+++ src/components/Buttons/NavigationButton.jsx
@@ -2,124 +2,112 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import { colors, typography } from '../../styles';
-// import ButtonLeftBorder from '../../assets/img/buttons/navigation-button-left-border.svg';
-// import ButtonRightBorder from '../../assets/img/buttons/navigation-button-right-border.svg';
+import styled, { css } from 'styled-components';
+import { colors } from '../../styles';
 
-const styles = {
-  activeButton: {
-    ...typography.buttons,
-    maxWidth: '200px',
-    textAlign: 'center',
-    height: '36px',
-    padding: '4px 16px',
-    position: 'relative',
-    display: 'flex',
-    alignItems: 'center',
-    lineHeight: 1.5,
-    letterSpacing: '2px',
-    fontWeight: 'bold',
-    boxSizing: 'border-box',
-    borderRadius: '1px',
-    border: `1px solid ${colors.white}`,
-    '&:hover': {
-      cursor: 'crosshair',
-    },
-  },
-  button: {
-    ...typography.buttons,
-    maxWidth: '200px',
-    textAlign: 'center',
-    height: '36px',
-    color: colors.white,
-    padding: '8px 16px 8px 24px',
-    position: 'relative',
-    display: 'flex',
-    alignItems: 'center',
-    lineHeight: 1.5,
-    letterSpacing: '2px',
-    fontWeight: 'bold',
-    boxSizing: 'border-box',
-    borderRadius: '1px',
-    borderTop: '1px dotted rgba(255,255,255,0.48)',
-    borderBottom: '1px dotted rgba(255,255,255,0.48)',
-    borderLeft: `1px solid ${colors.white}`,
-    borderRight: `1px solid ${colors.white}`,
-    '&:before': {
-      content: 'no-close-quote',
-      display: 'block',
-      position: 'absolute',
-      top: '6px',
-      left: '16px',
-      width: '24px',
-      height: '24px',
-      backgroundColor: 'rgba(73, 221, 186, 0.12)',
-    },
-    '&:hover': {
-      ...typography.buttons,
-      cursor: 'crosshair',
-      maxWidth: '200px',
-      textAlign: 'center',
-      height: '36px',
-      padding: '8px 16px 8px 24px',
-      position: 'relative',
-      display: 'flex',
-      lineHeight: 1.5,
-      letterSpacing: '2px',
-      fontWeight: 'bold',
-      alignItems: 'center',
-      boxSizing: 'border-box',
-      borderRadius: '1px',
-      border: `1px solid ${colors.white}`,
-      '&:before': {
-        display: 'none',
-      },
-      '& $textWrapper': {
-        backgroundColor: '#49ddba',
-        color: colors.black,
-      },
-    },
-  },
-  active: {
-    backgroundColor: colors.primary,
-    color: colors.black,
-  },
-  activeTextWrapper: {
-    padding: '0 4px',
-    backgroundColor: '#49ddba',
-  },
-  textWrapper: {
-  },
-  center: {
-    textAlign: 'center',
-  },
-};
+const Label = styled.span``;
+const LabelWrapper = styled.div``;
+const ButtonWrapper = styled.div`
+  font-size: 16px;
+  max-width: 200px;
+  text-align: center;
+  height: 36px;
+  color: ${colors.white};
+  padding: 8px 16px 8px 24px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  line-height: 1.5;
+  letter-spacing: 2px;
+  font-weight: bold;
+  box-sizing: border-box;
+  border-radius: 1px;
+  border-top: 1px dotted rgba(255,255,255,0.48);
+  border-bottom: 1px dotted rgba(255,255,255,0.48);
+  border-left: 1px solid ${colors.white};
+  border-right: 1px solid ${colors.white};
+  
+  &:before {
+    content: no-close-quote;
+    display: block;
+    position: absolute;
+    top: 6px;
+    left: 16px;
+    width: 24px;
+    height: 24px;
+    background-color: rgba(73, 221, 186, 0.12);
+  }
+
+  ${(props) => !props.disabled && css`
+    &:hover {
+      cursor: crosshair;
+      border: 1px solid ${colors.white};
+
+      &:before {
+        display: none;
+      }
+
+      ${LabelWrapper} {
+        background-color: #49ddba;
+        color: ${colors.black};
+      };
+    }
+  `}
+
+  ${(props) => props.active && css`
+    padding: 4px 16px;
+    border: 1px solid ${colors.white};
+
+    ${LabelWrapper} {
+      background-color: #49ddba;
+      padding: 0 4px;
+
+      ${Label} {
+        background-color: ${colors.primary};
+        color: ${colors.black};
+      };
+    }
+  `}
+
+  ${(props) => props.disabled && css`
+    opacity: 0.24;
+    pointer-events: none;
+  `}
+
+  ${(props) => props.hidden && css`
+    display: none;
+  `}
+`;
 
 const NavigationButton = ({ ...props }) => {
   const {
     children,
-    classes,
     active,
+    disabled,
+    hidden,
     onClick,
   } = props;
 
   return (
-    <div className={active ? classes.activeButton : classes.button} onClick={onClick}>
-      {/* <img className={classes.leftBorder} src={ButtonLeftBorder} alt='' />
-        <img className={classes.rightBorder} src={ButtonRightBorder} alt='' /> */}
-      <div className={active ? classes.activeTextWrapper : classes.textWrapper}>
-        <span className={`${classes.text} ${active ? classes.active : ''}`}>{children}</span>
-      </div>
-    </div>
+    <ButtonWrapper
+      hidden={hidden}
+      disabled={disabled}
+      active={active}
+      onClick={onClick}
+    >
+      <LabelWrapper>
+        <Label>{children}</Label>
+      </LabelWrapper>
+    </ButtonWrapper>
   );
 };
 
 NavigationButton.propTypes = {
   children: PropTypes.any,
-  classes: PropTypes.any,
   onClick: PropTypes.func,
   active: PropTypes.bool,
+  hidden: PropTypes.bool,
+  disabled: PropTypes.bool,
 };
 
-export default withStyles(styles)(NavigationButton);
+export default NavigationButton;
