46cd0be030d97ce1aad68487f0df975bdd3a8924
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-12-23 15:42:39 +0200

    Add project specific Heading component

---
+++ src/components/Steps/Project/Heading.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Grid from '@material-ui/core/Grid';
+import Image from '../Image';
+
+const ImageWrapper = styled(Grid)`
+  && {
+    max-width: 60%;
+    margin: 0 auto;
+  }
+`;
+
+const Key = styled.div`
+  font-weight: bold;
+`;
+const Info = styled(Grid)`
+  font-family: 'Space Mono';
+`;
+
+const Heading = ({ imageSrc, project }) => (
+  <Grid container>
+    <Info item xs={2}>
+      <div>
+        <img src={project.icon} width={36} alt="" />
+      </div>
+      <div>
+        <Key>Project name:</Key>
+        {' '}
+        {project.name}
+      </div>
+      <div>
+        <Key>Duration:</Key>
+        {' '}
+        {project.duration}
+      </div>
+      <div>
+        <Key>Role:</Key>
+        {' '}
+        {project.role}
+      </div>
+    </Info>
+    <ImageWrapper item xs={8}>
+      <Image src={imageSrc} />
+    </ImageWrapper>
+    <Grid item xs={2} />
+  </Grid>
+);
+
+Heading.propTypes = {
+  imageSrc: PropTypes.string.isRequired,
+  project: PropTypes.any.isRequired,
+};
+
+export default Heading;
