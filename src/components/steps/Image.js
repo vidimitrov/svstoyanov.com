6bb2b06a75e2bdb53aa070e746fe337efc2afa42
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-12-23 15:42:55 +0200

    Fix the styles of the Image component

--- src/components/steps/Image.js
+++ src/components/steps/Image.js
@@ -3,12 +3,10 @@ import styled from 'styled-components';
 const ImageStep = styled.img`
   width: 100%;
   height: 100%;
-  border: 3px solid #02a0a7;
-  border-radius: 3px;
 
-  @media (max-width: 420px) {
+  /* @media (max-width: 420px) {
     width: 150px;
-  }
+  } */
 `;
 
 export default ImageStep;
