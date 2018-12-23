38a8d0e73afdf6a5405c65bd32257b9559b58dde
Veselin Dimitrov <vidimi7rov@gmail.com>
2018-12-23 15:43:21 +0200

    Make sendEmail function a named export

--- src/lib/email.js
+++ src/lib/email.js
@@ -2,7 +2,7 @@ import request from 'superagent';
 
 const API_URL = 'https://svstoyanov.herokuapp.com';
 
-const sendEmail = async (to, message) => {
+export const sendEmail = async (to, message) => {
   const res = await request
     .post(`${API_URL}/email/send`)
     .send({ email: to, message });
