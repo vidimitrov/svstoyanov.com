ba6ba94ea3a03d803d26e3a4c2d3e3869e75afee
Veselin Dimitrov <vidimi7rov@gmail.com>
2019-01-15 17:39:20 +0200

    Remove unnecessary box-shadow

--- src/lib/ChatBot/ChatBotContainer.jsx
+++ src/lib/ChatBot/ChatBotContainer.jsx
@@ -4,20 +4,19 @@ import defaultTheme from './theme';
 const ChatBotContainer = styled.div`
   background: ${({ theme }) => theme.background};
   border-radius: 10px;
-  box-shadow: 0 12px 24px 0 rgba(0, 0, 0, 0.15);
   font-family: ${({ theme }) => theme.fontFamily};
   overflow: hidden;
-  position: ${props => props.floating ? 'fixed' : 'relative'};
-  bottom: ${props => props.floating ? '32px' : 'initial'};
-  right: ${props => props.floating ? '32px' : 'initial'};
+  position: ${props => (props.floating ? 'fixed' : 'relative')};
+  bottom: ${props => (props.floating ? '32px' : 'initial')};
+  right: ${props => (props.floating ? '32px' : 'initial')};
   width: ${props => props.width};
   z-index: 999;
-  transform: ${props => props.opened ? 'scale(1)' : 'scale(0)'};
+  transform: ${props => (props.opened ? 'scale(1)' : 'scale(0)')};
   transform-origin: bottom right;
   transition: transform .3s ease;
 
   @media screen and (max-width: 568px) {
-    border-radius: ${props => props.floating ? '0' : ''};
+    border-radius: ${props => (props.floating ? '0' : '')};
     bottom: 0;
     height: 100%;
     right: 0;
