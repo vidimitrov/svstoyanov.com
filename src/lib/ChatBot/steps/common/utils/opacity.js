34577ea2013ad88494fadd8b4f906c87108536fa
Veselin Dimitrov <vidimi7rov@gmail.com>
2019-01-16 17:06:03 +0200

    Fix opacity calculation logic

--- src/lib/ChatBot/steps/common/utils/opacity.js
+++ src/lib/ChatBot/steps/common/utils/opacity.js
@@ -1,3 +1,4 @@
-const LIMIT = 450;
+const OFFSET = 550;
+const STRANGE = 1.1;
 
-export const calculateOpacity = el => 1 - (0.8 - el.getBoundingClientRect().y / LIMIT);
+export const calculateOpacity = el => 1 - (STRANGE - el.getBoundingClientRect().bottom / OFFSET);
