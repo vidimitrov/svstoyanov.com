c57b023b9fd17c3ca724cff44c03d04b242306f6
Veselin Dimitrov <vidimi7rov@gmail.com>
2019-03-05 14:22:32 +0200

    Box shadow of the chat container WIP

--- src/lib/ChatBot/steps/common/utils/opacity.js
+++ src/lib/ChatBot/steps/common/utils/opacity.js
@@ -3,4 +3,6 @@ import { isMobile } from '../../../../viewport';
 const OFFSET = isMobile() ? 250 : 550;
 const STRANGE = 1.1;
 
-export const calculateOpacity = el => 1 - (STRANGE - el.getBoundingClientRect().bottom / OFFSET);
+// export const calculateOpacity = el => 1 - (STRANGE - el.getBoundingClientRect().bottom / OFFSET);
+
+export const calculateOpacity = el => 1;
