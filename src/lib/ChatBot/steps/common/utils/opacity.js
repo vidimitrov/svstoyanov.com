e903583754dca46c3cfe0a1da31ed8875e2f09fc
Stoyan <sstoyanov08@gmail.com>
2019-03-06 15:05:30 +0200

    Revert back the opacity lib

--- src/lib/ChatBot/steps/common/utils/opacity.js
+++ src/lib/ChatBot/steps/common/utils/opacity.js
@@ -3,6 +3,4 @@ import { isMobile } from '../../../../viewport';
 const OFFSET = isMobile() ? 250 : 550;
 const STRANGE = 1.1;
 
-// export const calculateOpacity = el => 1 - (STRANGE - el.getBoundingClientRect().bottom / OFFSET);
-
-export const calculateOpacity = el => 1;
+export const calculateOpacity = el => 1 - (STRANGE - el.getBoundingClientRect().bottom / OFFSET);
