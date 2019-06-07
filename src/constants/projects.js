984e0dc1da6b8248e20dfa47f3dbfe114df2b5a2
Veselin Dimitrov <veselin@toptal.com>
2019-06-07 23:36:01 +0300

    Update project constants

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,4 +1,17 @@
-export default [{
+import historyParkFullImage from '../assets/img/projects/history-park/history-park-full-image.png';
+import historyParkHeading from '../assets/img/projects/history-park/history-park-heading.png';
+import historyParkReviewAuthorAvatar from '../assets/img/projects/history-park/history-park-avatar.png';
+import historyParkLogo from '../assets/img/projects/history-park/history-park-logo.png';
+
+export const TYPES = {
+  Image: 'image',
+  FullWidthImage: 'full-width-image',
+  Text: 'text',
+  Section: 'section',
+  Review: 'review',
+};
+
+export const SnappProject = {
   id: 0,
   name: 'Snapp Builder',
   headline: 'Its all about building apps',
@@ -16,7 +29,9 @@ export default [{
   peopleInterest: 0.24,
   trustFactor: 0.14,
   realtimeboardLink: 'https://realtimeboard.com/b/12345',
-}, {
+};
+
+export const SeembaProject = {
   id: 1,
   name: 'Seemba',
   headline: 'Its all about online presence',
@@ -34,107 +49,54 @@ export default [{
   peopleInterest: 0.24,
   trustFactor: 0.14,
   realtimeboardLink: 'https://realtimeboard.com/b/12345',
-}, {
-  id: 2,
+};
+
+export const HistoryParkProject = {
+  trigger: '7d538274-a6af-41af-bd3d-371edc1bb0ab',
+  id: '2',
   name: 'History Park',
-  headline: 'Its all about history',
+  headline: 'History Park - Its all about history',
   shortDescription: 'This is some simple and short description of the project. Now I am testing the rendering animation when the text size is always the same! 3',
   description: 'Characters that have had created the big part of the European History. Well guess what Bulgaria takes role here in on Balkans with it nation which is several thousand years old. This is History park, it brings Bulgarian and partial Roman history by presenting it in a interactive way and taking you centuries and decades ago. People in the park will be able to experience this time travel, ride horses and shoot with a bow while riding, eat ancient kitchen, leave like the old days, even fight on the arena.',
-  duration: '1_MONTH',
-  review: {
+  logo: historyParkLogo,
+  metadata: {
+    duration: '1_MONTH',
+    peopleInterest: 0.24,
+    trustFactor: 0.14,
+    realtimeboardLink: 'https://realtimeboard.com/b/12345',
+  },
+  sections: [{
+    type: TYPES.Image,
+    src: historyParkHeading,
+  }, {
+    type: TYPES.Text,
+    content: 'Characters that have had created the big part of the European History. Well guess what Bulgaria takes role here in on Balkans with it nation which is several thousand years old. This is History park, it brings Bulgarian and partial Roman history by presenting it in a interactive way and taking you centuries and decades ago. People in the park will be able to experience this time travel, ride horses and shoot with a bow while riding, eat ancient kitchen, leave like the old days, even fight on the arena.',
+  }, {
+    type: TYPES.Section,
+    heading: 'Problem',
+    content: 'Imagine Disney Land but instead of the cartoon characters real ones. Characters that have had created the big part of the European History.',
+    bulletPoints: [
+      'Imagine Disney Land but instead of the cartoon characters real ones. Characters that have had created the big part of the European History.',
+      'Imagine Disney Land but instead of the cartoon characters real ones. Characters that have had created the big part of the European History.',
+    ],
+  }, {
+    type: TYPES.FullWidthImage,
+    src: historyParkFullImage,
+  }, {
+    type: TYPES.Review,
+    headline: 'Georgi Detelinov - client',
     companyName: 'Together Bulgaria',
-    avatar: '',
     content: 'Working with Stoyan was just an awesome experience. He invested time to learn about out wants and need , he also took time to learn everything needed fo the project Historical Park so he could make the best website for it He was honest and transparent about the process and kept us involved all the time. It was up to Stoyan to really translate everything we talked about in to the website design and to find the best one for this big project.. The end result was just awesome, he did what we had as an idea and done it at the best way. Looking forward to work with Stoyan again.',
     representative: 'GEORGI_DETELINOV',
     representativeRole: 'FINANCE_CONSULTANT',
-  },
-  landingImage: null,
-  metadata: [{
-    displayName: '',
-    value: null,
+    avatar: historyParkReviewAuthorAvatar,
+  }, {
+    type: TYPES.Text,
+    content: 'Characters that have had created the big part of the European History. Well guess what Bulgaria takes role here in on Balkans with it nation which is several thousand years old. This is History park, it brings Bulgarian and partial Roman history by presenting it in a interactive way and taking you centuries and decades ago. People in the park will be able to experience this time travel, ride horses and shoot with a bow while riding, eat ancient kitchen, leave like the old days, even fight on the arena.',
+  }, {
+    type: TYPES.FullWidthImage,
+    src: historyParkFullImage,
   }],
-  peopleInterest: 0.24,
-  trustFactor: 0.14,
-  realtimeboardLink: 'https://realtimeboard.com/b/12345',
-  problem: {
-    type: 'bullet-points',
-    description: 'Imagine Disney Land but instead of the cartoon characters real ones. Characters that have had created the big part of the European History.',
-    bulletPoints: [
-      'Imagine Disney Land but instead of the cartoon characters real ones. Characters that have had created the big part of the European History.',
-      'ad Imagine Disney Land but instead of the cartoon characters real ones. Characters that have had created the big part of the European History.',
-    ],
-  },
-  images: ['image_1_uri', 'image_1_uri', 'image_1_uri'],
-}];
+};
 
-
-/* Example project steps
-
-  {
-    id: '1',
-    component: (
-      <Heading
-        imageSrc={historyParkHeadingImg}
-        project={{
-          name: 'History Park',
-          duration: '3 months',
-          role: 'UX Designer, Strategiest & Researcher',
-          icon: historyParkIcon,
-        }}
-      />
-    ),
-    trigger: '2',
-    style: {
-      width: '100%',
-      backgroundColor: 'transparent',
-      padding: 0,
-    },
-  }, {
-    id: '2',
-    component: (
-      <ProblemSection
-        items={[{
-          key: '1',
-          type: 'text',
-          content: 'Imagine Disney Land but instead of the cartoon characters real ones.',
-        }, {
-          key: '2',
-          type: 'bullet',
-          content: 'Imagine Disney Land but instead of the cartoon characters real ones.',
-        }, {
-          key: '3',
-          type: 'bullet',
-          content: 'Imagine Disney Land but instead of the cartoon characters real ones.',
-        }, {
-          key: '4',
-          type: 'image',
-          imageSrc: stoyanBushcraftingImg,
-        }]}
-      />
-    ),
-    trigger: '3',
-  }, {
-    id: '3',
-    component: (
-      <Testimonials
-        title="Georgi - customer"
-        content="Working with Stoyan was just an awesome experience."
-        avatarSrc={historyParkAvatar}
-      />
-    ),
-    trigger: '4',
-  }, {
-    id: '4',
-    component: (
-      <Image
-        src={historyParkFullImage}
-      />
-    ),
-    style: {
-      padding: 0,
-      margin: '32px 0 0',
-      backgroundColor: 'transparent',
-    },
-  },
-
-*/
+export default [SnappProject, SeembaProject, HistoryParkProject];
