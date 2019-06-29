7d3900d89370dbdde7807f32850e2a02193e458a
Stoyan <sstoyanov08@gmail.com>
2019-06-29 15:04:41 +0300

    image step auto scroll fixed

--- src/components/Steps/Image.js
+++ src/components/Steps/Image.js
@@ -1,9 +1,16 @@
 import styled from 'styled-components';
+import { breakpoints } from '../../styles';
 
 const ImageStep = styled.img`
   margin: 40px 0;
   width: 100%;
-  height: 100%;
+  /* Fixed height for mobile, because of the scroll problem  */
+  height: 200px;
+
+  ${breakpoints.md} {
+    /* Fixed height for desktop, because of the scroll problem  */
+    height: 500px;
+  }
 `;
 
 export default ImageStep;
